5db263f45c7e07b2b41ca58f040e16271014ae56
EqUiNoX <equinox.teamresurgent@gmail.com>
2025-04-07 18:35:45 -0600

    make more c# friendly

--- main.cpp
+++ main.cpp
@@ -10,22 +10,24 @@ int CalcDecompressedSize(const uint8_t* src, const uint32_t src_size, uint32_t* 
     return lzx_calc_decompress_size(src, src_size, decompressed_size);
 }
 
-int Decompress(const uint8_t* src, const uint32_t src_size, uint8_t** dest, uint32_t* dest_size, uint32_t* decompressed_size)
+int Decompress(const uint8_t* src, const uint32_t src_size, uint8_t** dest, uint32_t* decompressed_size)
 {
-    return lzx_decompress(src, src_size, dest, dest_size, decompressed_size);
+    uint8_t* buffer = nullptr;
+    int result = lzx_decompress(src, src_size, &buffer, 0, decompressed_size);
+    *dest = buffer; 
+    return result;
 }
 
 int Compress(const uint8_t* src, const uint32_t src_size, uint8_t** dest, uint32_t* compressed_size)
 {
-    return lzx_compress(src, src_size, dest, compressed_size);
+    uint8_t* buffer = nullptr;
+    int result = lzx_compress(src, src_size, &buffer, compressed_size);
+    *dest = buffer; 
+    return result;
 }
 
-uint8_t* AllocateBuffer(uint32_t buffer_size)
-{
-    return (uint8_t*)malloc(buffer_size);
-}
-
-void FreeBuffer(uint8_t* buffer)
+int FreeBuffer(uint8_t* buffer)
 {
     free(buffer);
+    return 0;
 }
--- main.h
+++ main.h
@@ -7,7 +7,6 @@
 #endif
 
 TR_EXPORT int CalcDecompressedSize(const uint8_t* src, const uint32_t src_size, uint32_t* decompressed_size);
-TR_EXPORT int Decompress(const uint8_t* src, const uint32_t src_size, uint8_t** dest, uint32_t* dest_size, uint32_t* decompressed_size);
+TR_EXPORT int Decompress(const uint8_t* src, const uint32_t src_size, uint8_t** dest, uint32_t* decompressed_size);
 TR_EXPORT int Compress(const uint8_t* src, const uint32_t src_size, uint8_t** dest, uint32_t* compressed_size);
-TR_EXPORT uint8_t* AllocateBuffer(uint32_t buffer_size);
-TR_EXPORT void FreeBuffer(uint8_t* buffer);
+TR_EXPORT int FreeBuffer(uint8_t* buffer);
